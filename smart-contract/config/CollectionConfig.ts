f6debd5139ebe04eb768d1b45e9c38e84dde7dbc
ShahTechno <zahid4it@gmail.com>
2023-04-01 02:39:36 +0400

    123

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -4,30 +4,30 @@ import * as Marketplaces from '../lib/Marketplaces';
 import whitelistAddresses from './whitelist.json';
 
 const CollectionConfig: CollectionConfigInterface = {
-  testnet: Networks.ethereumTestnet,
-  mainnet: Networks.ethereumMainnet,
+  testnet: Networks.polygonTestnet,
+  mainnet: Networks.polygonMainnet,
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'PitbullDogs',
+  tokenName: 'Pitbull NFT CLub',
+  tokenSymbol: 'PITBULL',
+  hiddenMetadataUri: 'ipfs://QmeprvHNA49V6JbGLd19agE1ucP4xv6Ua71pPAWzSJHumw/hidden.json',
+  maxSupply: 30,
   whitelistSale: {
-    price: 0.05,
-    maxMintAmountPerTx: 1,
-  },
-  preSale: {
-    price: 0.07,
+    price: 0.005,
     maxMintAmountPerTx: 2,
   },
-  publicSale: {
-    price: 0.09,
-    maxMintAmountPerTx: 5,
+  preSale: {
+    price: 0.007,
+    maxMintAmountPerTx: 4,
   },
-  contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  publicSale: {
+    price: 0.009,
+    maxMintAmountPerTx: 10,
+  },
+  contractAddress: "0x57861f9D0D11e58E2e9240AC8e327f04cBf88B15",
+  marketplaceIdentifier: 'Pitbull NFT CLub',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
